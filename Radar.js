3b71ddad7f5bbf78431bf003dcdf7bdd0afbe7e0
Aaron Bell <aaron.bell@gmail.com>
2014-02-06 06:43:13 -0500

    fully working radar, better than original : )

--- Radar.js
+++ Radar.js
@@ -2,6 +2,7 @@ var Radar = {};
 
 Radar.RESOLUTION_X = 200;
 Radar.RESOLUTION_Z = 200;
+Radar.RANGE = 20000;
 
 Radar.CENTER_X = Math.floor(Radar.RESOLUTION_X / 2);
 Radar.CENTER_Z = Math.floor(Radar.RESOLUTION_Z / 2);
@@ -42,6 +43,8 @@ Radar.blip = function(x, z)
   Radar.canvasContext.fillRect(x - Radar.BLIP_RADIUS, z - Radar.BLIP_RADIUS, Radar.BLIP_RADIUS * 2, Radar.BLIP_RADIUS * 2);  
 }
 
+// TODO try THREE.Math.mapLinear?
+// This will scale the radar to show the entire Grid.
 Radar.translatePosition = function(worldx, worldz)
 {
   var x = (worldx / Grid.MAX_X) * Radar.RESOLUTION_X;
@@ -49,29 +52,47 @@ Radar.translatePosition = function(worldx, worldz)
   return new THREE.Vector2(x, z);
 }
 
+// This limits the radar to only showing Radar.RANGE.
+Radar.translatePositionByRange = function(xPos, zPos)
+{
+  var x = (xPos / Radar.RANGE) * Radar.RESOLUTION_X;
+  var z = (zPos / Radar.RANGE) * Radar.RESOLUTION_Z;
+  return new THREE.Vector2(x, z);
+}
+
+Radar.render = function(worldx, worldz)
+{
+  // translate so player is at origin
+  var xRelativeToPlayer = worldx - Player.position.x;
+  var zRelativeToPlayer = worldz - Player.position.z;
+
+  // rotate, http://en.wikipedia.org/wiki/Rotation_(mathematics)#Matrix_algebra
+  var x = (xRelativeToPlayer * Math.cos(Player.rotation.y)) - (zRelativeToPlayer * Math.sin(Player.rotation.y));
+  var z = (xRelativeToPlayer * Math.sin(Player.rotation.y)) + (zRelativeToPlayer * Math.cos(Player.rotation.y));
+
+  // scale for radar range
+  var radarPos = Radar.translatePositionByRange(x, z);
+
+  // paint relative to center (player)
+  Radar.blip(Radar.CENTER_X + radarPos.x, Radar.CENTER_Z + radarPos.y);
+}
+
 Radar.update = function()
 {
   Radar.canvasContext.clearRect(0, 0, Radar.RESOLUTION_X, Radar.RESOLUTION_Z);
-
-  // player
   Radar.canvasContext.fillStyle = "#FFFFFF";
-  //Radar.blip(Radar.CENTER_X, Radar.CENTER_Z);
-  var playerPosition = Radar.translatePosition(Player.position.x, Player.position.z);
-  Radar.blip(playerPosition.x, playerPosition.y);
+  Radar.render(Player.position.x, Player.position.z);
 
-  // shots
-  Radar.canvasContext.fillStyle = "#666666";
   for (var i = 0; i < actors.length; i++)
   {
-    var actorPosition = Radar.translatePosition(actors[i].position.x, actors[i].position.z);
-    Radar.blip(actorPosition.x, actorPosition.y);
-  }
-
-  // enemy
-  if (Enemy.isAlive)
-  {
-    Radar.canvasContext.fillStyle = "#FF0000";
-    var enemyPosition = Radar.translatePosition(Enemy.position.x, Enemy.position.z);
-    Radar.blip(enemyPosition.x, enemyPosition.y);
+    if (actors[i].isAlive)
+    {
+      Radar.canvasContext.fillStyle = "#FF0000";
+    }
+    else
+    {
+      Radar.canvasContext.fillStyle = "#666666";
+    }
+    Radar.render(actors[i].position.x, actors[i].position.z);
   }
 }
