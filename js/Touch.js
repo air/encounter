413700ca24fea738de6e2d80e2e9efa710441fc6
Aaron Bell <aaron.bell@gmail.com>
2014-04-23 19:45:43 -0400

    epic refactor, press/unpress references not working yet

--- js/Touch.js
+++ js/Touch.js
@@ -9,199 +9,182 @@ Touch.fireButton = null;
 
 Touch.init = function()
 {
-  
-  Touch.initTouch();
-}
-
-Touch.addHandlersForTouch = function(div, handler)
-{
-  div.addEventListener('touchend', handler);
-  div.addEventListener('touchleave', handler);
-  div.addEventListener('touchmove', function(event) {
-    event.preventDefault();
-    var touch = event.changedTouches[0];
-    log('touch at ' + touch.clientX + ', ' + touch.clientY);
-    log('element is ' + document.elementFromPoint(touch.clientX, touch.clientY).id);
-  });
-}
-
-Touch.initTouch = function()
-{
   // disable dragging
   // FIXME this shouldn't be needed but we go a few px off bottom of screen, which enables drag
   document.addEventListener('touchmove', function(event) {
     event.preventDefault();
   });
 
-  // touch fire button
-  var fireButton = document.createElement('div');
-  fireButton.id = 'fireButton';
+  Touch.initFireButton();
 
-  fireButton.style.cssText = Touch.CONTROLS_CSS;
-  fireButton.style.width = '40%';
-  fireButton.style.height = (Touch.DPAD_BUTTON_HEIGHT_PERCENT * 3) + '%';
-  fireButton.style.position = 'absolute';
-  fireButton.style.bottom = '0px';
-  fireButton.style.right = '0px';
-  fireButton.addEventListener('touchstart', function(event) {
+  Touch.dpad['upleft'] = Touch.createDPadButton('upleft');
+  Touch.dpad['upleft'].style.bottom = (Touch.DPAD_BUTTON_HEIGHT_PERCENT * 2) + '%';
+  Touch.dpad['upleft'].press = function() {
+    event.preventDefault();
+    Controls.current.moveForward = true;
+    Controls.current.turnLeft = true;
+  };
+  Touch.dpad['upleft'].unpress = function() {
+    event.preventDefault();
+    Controls.current.moveForward = false;
+    Controls.current.turnLeft = false;
+  };
+
+  Touch.dpad['up'] = Touch.createDPadButton('up');
+  Touch.dpad['up'].style.bottom = (Touch.DPAD_BUTTON_HEIGHT_PERCENT * 2) + '%';
+  Touch.dpad['up'].style.left = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
+  Touch.dpad['up'].press = function() {
+    event.preventDefault();
+    Controls.current.moveForward = true;
+  };
+  Touch.dpad['up'].unpress = function() {
+    event.preventDefault();
+    Controls.current.moveForward = false;
+  };
+
+  Touch.dpad['upright'] = Touch.createDPadButton('upright');
+  Touch.dpad['upright'].style.bottom = (Touch.DPAD_BUTTON_HEIGHT_PERCENT * 2) + '%';
+  Touch.dpad['upright'].style.left = (Touch.DPAD_BUTTON_WIDTH_PERCENT * 2) + '%';
+  Touch.dpad['upright'].press = function() {
+    event.preventDefault();
+    Controls.current.moveForward = true;
+    Controls.current.turnRight = true;
+  };
+  Touch.dpad['upright'].unpress = function() {
+    event.preventDefault();
+    Controls.current.moveForward = false;
+    Controls.current.turnRight = false;
+  };
+
+  Touch.dpad['left'] = Touch.createDPadButton('left');
+  Touch.dpad['left'].style.bottom = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
+  Touch.dpad['left'].press = function() {
+    event.preventDefault();
+    Controls.current.turnLeft = true;
+  };
+  Touch.dpad['left'].unpress = function() {
+    event.preventDefault();
+    Controls.current.turnLeft = false;
+  };
+
+  Touch.dpad['right'] = Touch.createDPadButton('right');
+  Touch.dpad['right'].style.bottom = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
+  Touch.dpad['right'].style.left = (Touch.DPAD_BUTTON_WIDTH_PERCENT * 2) + '%';
+  Touch.dpad['right'].press = function() {
+    event.preventDefault();
+    Controls.current.turnRight = true;
+  };
+  Touch.dpad['right'].unpress = function() {
+    event.preventDefault();
+    Controls.current.turnRight = false;
+  };
+
+  Touch.dpad['downleft'] = Touch.createDPadButton('downleft');
+  Touch.dpad['downleft'].press = function() {
+    event.preventDefault();
+    Controls.current.moveBackward = true;
+    Controls.current.turnLeft = true;
+  };
+  Touch.dpad['downleft'].unpress = function() {
+    event.preventDefault();
+    Controls.current.moveBackward = false;
+    Controls.current.turnLeft = false;
+  };
+
+  Touch.dpad['down'] = Touch.createDPadButton('down');
+  Touch.dpad['down'].style.left= Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
+  Touch.dpad['down'].press = function() {
+    event.preventDefault();
+    Controls.current.moveBackward = true;
+  };
+  Touch.dpad['down'].unpress = function() {
+    event.preventDefault();
+    Controls.current.moveBackward = false;
+  };
+
+  Touch.dpad['downright'] = Touch.createDPadButton('downright');
+  Touch.dpad['downright'].style.left = (Touch.DPAD_BUTTON_WIDTH_PERCENT * 2) + '%';
+  Touch.dpad['downright'].press = function() {
+    event.preventDefault();
+    Controls.current.moveBackward = true;
+    Controls.current.turnRight = true;
+  };
+  Touch.dpad['downright'].unpress = function() {
+    event.preventDefault();
+    Controls.current.moveBackward = false;
+    Controls.current.turnRight = false;
+  };
+}
+
+// DPad buttons are divs with explicit press/unpress functions.
+// This function assumes you live at bottom-left of the screen.
+// This function provides dummy press() and unpress() function which must be replaced.
+Touch.createDPadButton = function(id)
+{
+  var button = document.createElement('div');
+  button.id = id;
+  button.style.cssText = Touch.CONTROLS_CSS;
+  button.style.width = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
+  button.style.height = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
+  button.style.position = 'absolute';
+  button.style.bottom = '0px';
+  button.style.left = '0px';
+
+  button.press = function() {
+    error('a dummy button press function wasn\'t replaced!');
+  };
+  button.unpress = function() {
+    error('a dummy button unpress function wasn\'t replaced!');
+  };
+
+  // press handler for basic touchstart case
+  button.addEventListener('touchstart', button.press);
+  // unpress handlers
+  button.addEventListener('touchend', button.unpress);
+  button.addEventListener('touchleave', button.unpress);
+
+  // FIXME add touchmove handler
+  //div.addEventListener('touchmove', function(event) {
+  //  event.preventDefault();
+  //  var touch = event.changedTouches[0];
+  //  log('touch at ' + touch.clientX + ', ' + touch.clientY);
+  //  log('element is ' + document.elementFromPoint(touch.clientX, touch.clientY).id);
+
+  document.body.appendChild(button);
+
+  return button;
+}
+
+// fire button is a bit simpler, no need for any touchmove for sliding fingers
+Touch.initFireButton = function()
+{
+  Touch.fireButton = document.createElement('div');
+  Touch.fireButton.id = 'fireButton';
+  Touch.fireButton.style.cssText = Touch.CONTROLS_CSS;
+  Touch.fireButton.style.width = '40%';
+  Touch.fireButton.style.height = (Touch.DPAD_BUTTON_HEIGHT_PERCENT * 3) + '%';
+  Touch.fireButton.style.position = 'absolute';
+  Touch.fireButton.style.bottom = '0px';
+  Touch.fireButton.style.right = '0px';
+
+  Touch.fireButton.press = function() {
     event.preventDefault();
     Keys.shooting = true;
-  });
-  Touch.addHandlersForTouch(fireButton, function(event) {
+  };
+  Touch.fireButton.unpress = function() {
     event.preventDefault();
     Keys.shooting = false;
-  });
-  document.body.appendChild(fireButton);
+  };
 
-  var dPadUpLeft = document.createElement('div');
-  dPadUpLeft.id = 'dPadUpLeft';
-  dPadUpLeft.style.cssText = Touch.CONTROLS_CSS;
-  dPadUpLeft.style.width = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
-  dPadUpLeft.style.height = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  dPadUpLeft.style.position = 'absolute';
-  dPadUpLeft.style.bottom = (Touch.DPAD_BUTTON_HEIGHT_PERCENT * 2) + '%';
-  dPadUpLeft.style.left = '0px';
-  dPadUpLeft.addEventListener('touchstart', function(event) {
-    event.preventDefault();
-    Controls.current.moveForward = true;
-    Controls.current.turnLeft = true;
+  Touch.fireButton.addEventListener('touchstart', function(event) {
+    Touch.fireButton.press();
   });
-  Touch.addHandlersForTouch(dPadUpLeft, function(event) {
-    event.preventDefault();
-    Controls.current.moveForward = false;
-    Controls.current.turnLeft = false;
+  Touch.fireButton.addEventListener('touchend', function(event) {
+    Touch.fireButton.unpress();
   });
-  document.body.appendChild(dPadUpLeft);
+  Touch.fireButton.addEventListener('touchleave', function(event) {
+    Touch.fireButton.unpress();
+  });
 
-  var dPadUp = document.createElement('div');
-  dPadUp.id = 'dPadUp';
-  dPadUp.style.cssText = Touch.CONTROLS_CSS;
-  dPadUp.style.width = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
-  dPadUp.style.height = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  dPadUp.style.position = 'absolute';
-  dPadUp.style.bottom = (Touch.DPAD_BUTTON_HEIGHT_PERCENT * 2) + '%';
-  dPadUp.style.left = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
-  dPadUp.addEventListener('touchstart', function(event) {
-    event.preventDefault();
-    Controls.current.moveForward = true;
-  });
-  Touch.addHandlersForTouch(dPadUp, function(event) {
-    event.preventDefault();
-    Controls.current.moveForward = false;
-  });
-  document.body.appendChild(dPadUp);
-
-  var dPadUpRight = document.createElement('div');
-  dPadUpRight.id = 'dPadUpRight';
-  dPadUpRight.style.cssText = Touch.CONTROLS_CSS;
-  dPadUpRight.style.width = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
-  dPadUpRight.style.height = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  dPadUpRight.style.position = 'absolute';
-  dPadUpRight.style.bottom = (Touch.DPAD_BUTTON_HEIGHT_PERCENT * 2) + '%';
-  dPadUpRight.style.left = (Touch.DPAD_BUTTON_WIDTH_PERCENT * 2) + '%';
-  dPadUpRight.addEventListener('touchstart', function(event) {
-    event.preventDefault();
-    Controls.current.moveForward = true;
-    Controls.current.turnRight = true;
-  });
-  Touch.addHandlersForTouch(dPadUpRight, function(event) {
-    event.preventDefault();
-    Controls.current.moveForward = false;
-    Controls.current.turnRight = false;
-  });
-  document.body.appendChild(dPadUpRight);
-
-  var dPadLeft = document.createElement('div');
-  dPadLeft.id = 'dPadLeft';
-  dPadLeft.style.cssText = Touch.CONTROLS_CSS;
-  dPadLeft.style.width = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
-  dPadLeft.style.height = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  dPadLeft.style.position = 'absolute';
-  dPadLeft.style.bottom = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  dPadLeft.style.left = '0px';
-  dPadLeft.addEventListener('touchstart', function(event) {
-    event.preventDefault();
-    Controls.current.turnLeft = true;
-  });
-  Touch.addHandlersForTouch(dPadLeft, function(event) {
-    event.preventDefault();
-    Controls.current.turnLeft = false;
-  });
-  document.body.appendChild(dPadLeft);
-
-  var dPadRight = document.createElement('div');
-  dPadRight.id = 'dPadRight';
-  dPadRight.style.cssText = Touch.CONTROLS_CSS;
-  dPadRight.style.width = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
-  dPadRight.style.height = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  dPadRight.style.position = 'absolute';
-  dPadRight.style.bottom = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  dPadRight.style.left = (Touch.DPAD_BUTTON_WIDTH_PERCENT * 2) + '%';
-  dPadRight.addEventListener('touchstart', function(event) {
-    event.preventDefault();
-    Controls.current.turnRight = true;
-  });
-  Touch.addHandlersForTouch(dPadRight, function(event) {
-    event.preventDefault();
-    Controls.current.turnRight = false;
-  });
-  document.body.appendChild(dPadRight);
-
-  var dPadDownLeft = document.createElement('div');
-  dPadDownLeft.id = 'dPadDownLeft';
-  dPadDownLeft.style.cssText = Touch.CONTROLS_CSS;
-  dPadDownLeft.style.width = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
-  dPadDownLeft.style.height = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  dPadDownLeft.style.position = 'absolute';
-  dPadDownLeft.style.bottom = '0px';
-  dPadDownLeft.style.left = '0px';
-  dPadDownLeft.addEventListener('touchstart', function(event) {
-    event.preventDefault();
-    Controls.current.moveBackward = true;
-    Controls.current.turnLeft = true;
-  });
-  Touch.addHandlersForTouch(dPadDownLeft, function(event) {
-    event.preventDefault();
-    Controls.current.moveBackward = false;
-    Controls.current.turnLeft = false;
-  });
-  document.body.appendChild(dPadDownLeft);
-
-  var dPadDown = document.createElement('div');
-  dPadDown.id = 'dPadDown';
-  dPadDown.style.cssText = Touch.CONTROLS_CSS;
-  dPadDown.style.width = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
-  dPadDown.style.height = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  dPadDown.style.position = 'absolute';
-  dPadDown.style.bottom = '0px';
-  dPadDown.style.left = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
-  dPadDown.addEventListener('touchstart', function(event) {
-    event.preventDefault();
-    Controls.current.moveBackward = true;
-  });
-  Touch.addHandlersForTouch(dPadDown, function(event) {
-    event.preventDefault();
-    Controls.current.moveBackward = false;
-  });
-  document.body.appendChild(dPadDown);
-
-  var dPadDownRight = document.createElement('div');
-  dPadDownRight.id = 'dPadDownRight';
-  dPadDownRight.style.cssText = Touch.CONTROLS_CSS;
-  dPadDownRight.style.width = Touch.DPAD_BUTTON_WIDTH_PERCENT + '%';
-  dPadDownRight.style.height = Touch.DPAD_BUTTON_HEIGHT_PERCENT + '%';
-  dPadDownRight.style.position = 'absolute';
-  dPadDownRight.style.bottom = '0px';
-  dPadDownRight.style.left = (Touch.DPAD_BUTTON_WIDTH_PERCENT * 2) + '%';
-  dPadDownRight.addEventListener('touchstart', function(event) {
-    event.preventDefault();
-    Controls.current.moveBackward = true;
-    Controls.current.turnRight = true;
-  });
-  Touch.addHandlersForTouch(dPadDownRight, function(event) {
-    event.preventDefault();
-    Controls.current.moveBackward = false;
-    Controls.current.turnRight = false;
-  });
-  document.body.appendChild(dPadDownRight);
+  document.body.appendChild(Touch.fireButton);
 }
