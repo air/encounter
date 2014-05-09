ea1bb6bfed92fd1ab94d81ee548cee04bf4cdb4a
Aaron Bell <aaron.bell@gmail.com>
2014-05-09 14:22:39 -0400

    working gulp setup. Ignored node_modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+// optionals
+var bump = require('gulp-bump');
 
-gulp.task('default', function () {
+gulp.task('test', function () {
+  return gulp.src('./js/*.js')
+});
 
+gulp.task('release', function () {
+  return gulp.src('package.json').pipe(bump({type:'major'})).pipe(gulp.dest('.'));
 });
