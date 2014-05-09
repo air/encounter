a86987c3c6c0e349e359147add5dd33f392f3995
Aaron Bell <aaron.bell@gmail.com>
2014-05-09 15:58:01 -0400

    released version 1.0.3

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,39 @@
 var gulp = require('gulp');
 // optionals
 var bump = require('gulp-bump');
+var git = require('gulp-git');
 
-gulp.task('test', function () {
+gulp.task('test', function() {
   return gulp.src('./js/*.js')
 });
 
-gulp.task('release', function () {
-  return gulp.src('package.json').pipe(bump({type:'major'})).pipe(gulp.dest('.'));
+// update package.json version
+gulp.task('bump-version', ['test'], function() {
+  return gulp.src('package.json')
+    .pipe(bump())
+    .pipe(gulp.dest('.'));
 });
+
+// commit package.json and push it
+gulp.task('release', ['bump-version'], function() {
+  var version = require('./package.json').version;
+
+  return gulp.src('.')
+    .pipe(git.commit('released version ' + version))
+    .pipe(git.push('origin', 'master'))
+    .end();
+});
+
+// merge master into gh-pages and push it
+gulp.task('publish', ['release'], function() {
+  var version = require('./package.json').version;
+
+  return gulp.src('.')
+    .pipe(git.checkout('gh-pages'))
+    .pipe(git.merge('master'))
+    .pipe(git.commit('merged master ' + version + ' to gh-pages'))
+    .pipe(git.push('origin', 'gh-pages'))
+    .end();
+});
+
+// a minimal gulp plugin at https://github.com/sindresorhus/gulp-size/blob/master/index.js
