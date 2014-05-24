28bb1507c6627d7800d418b2e766931a12e2b5a5
Aaron Bell <aaron.bell@gmail.com>
2014-05-24 15:52:03 -0400

    black obelisks

--- js/Obelisk.js
+++ js/Obelisk.js
@@ -6,8 +6,7 @@ Obelisk.HEIGHT = 100;
 Obelisk.RADIUS = 40;
 // topRadius, bottomRadius, height, segments, heightSegments
 Obelisk.GEOMETRY = new THREE.CylinderGeometry(Obelisk.RADIUS, Obelisk.RADIUS, Obelisk.HEIGHT, 16, 1, false);
-Obelisk.MATERIAL = MATS.normal; // see also MATS.wireframe.clone();
-//Obelisk.MATERIAL.color = 0x000000;
+Obelisk.MATERIAL = new THREE.MeshBasicMaterial({ color: C64.black });
 
 Obelisk.newInstance = function()
 {
