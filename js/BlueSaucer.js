22a904d35f1cfddc7aff955510e3c5c0988704ad
Aaron Bell <aaron.bell@gmail.com>
2014-07-07 17:39:33 -0400

    blue saucer alternates materials

--- js/BlueSaucer.js
+++ js/BlueSaucer.js
@@ -5,7 +5,8 @@
 var BlueSaucer = Saucer.newInstance();
 
 BlueSaucer.RADIUS = Saucer.RADIUS; // need a copy for Shot.collideWithShips
-BlueSaucer.MATERIAL = new THREE.MeshBasicMaterial({ color: C64.cyan });
+BlueSaucer.MATERIAL1 = new THREE.MeshBasicMaterial({ color: C64.cyan });
+BlueSaucer.MATERIAL2 = new THREE.MeshBasicMaterial({ color: C64.lightgrey });
 BlueSaucer.SHOT_MATERIAL1 = new THREE.MeshBasicMaterial({ color: C64.cyan });
 BlueSaucer.SHOT_MATERIAL2 = new THREE.MeshBasicMaterial({ color: C64.lightgrey });
 
@@ -22,4 +23,22 @@ BlueSaucer.shoot = function()
   var shot = Shot.newInstance(BlueSaucer, BlueSaucer.position, BlueSaucer.rotation, BlueSaucer.SHOT_MATERIAL1, BlueSaucer.SHOT_MATERIAL2);
   State.actors.push(shot);
   scene.add(shot);
+}
+
+// override Saucer
+BlueSaucer.update = function(timeDeltaMillis)
+{
+  BlueSaucer.material = (BlueSaucer.material === BlueSaucer.MATERIAL1 ? BlueSaucer.MATERIAL2 : BlueSaucer.MATERIAL1);
+
+  switch(this.state)
+  {
+    case Saucer.STATE_WAITING:
+      BlueSaucer.updateWaiting(timeDeltaMillis);
+      break;
+    case Saucer.STATE_MOVING:
+      BlueSaucer.updateMoving(timeDeltaMillis);
+      break;
+    default:
+      error('unknown Saucer state: ' + this.state);
+  } 
 }
