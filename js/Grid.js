d8b2fb8f3d647e1c2c23794e6a4305a45e108a30
Aaron Bell <aaron.bell@gmail.com>
2022-02-23 08:25:24 -0500

    Grid

--- js/Grid.js
+++ js/Grid.js
@@ -1,9 +1,13 @@
-import { log, error, panic } from '/js/UTIL.js';
+import { log, error, panic, random } from '/js/UTIL.js';
+import * as Ground from '/js/Ground.js';
+import * as Physics from '/js/Physics.js';
+import * as Player from '/js/Player.js';
+import * as MY3 from '/js/MY3.js';
+import * as Obelisk from '/js/Obelisk.js';
+import * as Encounter from '/js/Encounter.js';
 
-// FIXME import
-//import { BufferGeometryUtils } from './lib/BufferGeometryUtils.js';
-
-var Grid = {};
+import { BufferGeometryUtils } from '/lib/BufferGeometryUtils.js';
+import * as THREE from '/lib/three.module.js'
 
 // the Grid is:
 // - sized depending on camera.far (set in Encounter.js, see Encounter.DRAW_DISTANCE)
@@ -12,127 +16,127 @@ var Grid = {};
 // - parent to the Ground plane
 // - a 2D viewport of fixed size looking down on an infinite grid, snapped to Grid.SPACING
 
-Grid.SPACING = 1000;
+export const SPACING = 1000;
 // need camera draw distance before we can calculate these:
-Grid.SIZE_SQUARE = null;  // how big the viewport is
-Grid.OBELISKS_PER_SIDE = null;
-Grid.TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE = null; // get this close to the viewport edge and we move the grid
+export var SIZE_SQUARE = null;  // how big the viewport is
+export var OBELISKS_PER_SIDE = null;
+export var TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE = null; // get this close to the viewport edge and we move the grid
 
 // state
-Grid.viewport = null; // use a THREE.Box2D as our viewport
-Grid.geometry = null; // we're going to merge all objects into a single Geometry
-Grid.mesh = null;
+export var viewport = null; // use a THREE.Box2D as our viewport
+export var geometry = null; // we're going to merge all objects into a single Geometry
+export var mesh = null;
 
 // the grid is not active during warp
-Grid.isActive = true;
+export var isActive = true;
 
-Grid.init = function()
+export function init()
 {
-  Grid.calculateConstants();
+  calculateConstants();
 
-  Grid.viewport = new THREE.Box2(new THREE.Vector2(0,0), new THREE.Vector2(Grid.SIZE_SQUARE, Grid.SIZE_SQUARE));
+  viewport = new THREE.Box2(new THREE.Vector2(0,0), new THREE.Vector2(SIZE_SQUARE, SIZE_SQUARE));
 
-  Grid.geometry = new THREE.BufferGeometry();
+  geometry = new THREE.BufferGeometry();
   var obeliskMesh = Obelisk.newMeshInstance(); // just need one of these as a cookie cutter
 
   // one-time loop to set up geometry
-  for (var rowIndex = 0; rowIndex < Grid.OBELISKS_PER_SIDE; rowIndex++)
+  for (var rowIndex = 0; rowIndex < OBELISKS_PER_SIDE; rowIndex++)
   {
-    for (var colIndex = 0; colIndex < Grid.OBELISKS_PER_SIDE; colIndex++)
+    for (var colIndex = 0; colIndex < OBELISKS_PER_SIDE; colIndex++)
     {
-      var xPos = colIndex * Grid.SPACING;
-      var zPos = rowIndex * Grid.SPACING;
+      var xPos = colIndex * SPACING;
+      var zPos = rowIndex * SPACING;
       // update the template mesh and merge it into Grid
       obeliskMesh.position.set(new THREE.Vector3(xPos, Obelisk.HEIGHT / 2, zPos));
-      Grid.geometry = THREE.BufferGeometryUtils.mergeBufferGeometries([Grid.geometry, obeliskMesh.geometry]);
+      geometry = THREE.BufferGeometryUtils.mergeBufferGeometries([geometry, obeliskMesh.geometry]);
     }
   }
 
-  Grid.mesh = new THREE.Mesh(Grid.geometry, Obelisk.MATERIAL);
+mesh = new THREE.Mesh(geometry, Obelisk.MATERIAL);
 
   // the final bit of setup for parent/child is delegated to Ground.init(), some unlovely coupling
 };
 
-Grid.calculateConstants = function()
+function calculateConstants()
 {
   // 1. Size the grid.
   // see how many SPACING intervals we need to cover 2x the draw distance, round that up, and multiply back to absolute size
-  Grid.SIZE_SQUARE = Math.ceil((camera.far * 2) / Grid.SPACING) * Grid.SPACING;
-  log('draw distance is ' + camera.far + ' so the grid viewport is a square of side ' + Grid.SIZE_SQUARE);
+  SIZE_SQUARE = Math.ceil((camera.far * 2) / SPACING) * SPACING;
+  log('draw distance is ' + camera.far + ' so the grid viewport is a square of side ' + SIZE_SQUARE);
 
   // 2. How many obelisks on a side?
-  Grid.OBELISKS_PER_SIDE = (Grid.SIZE_SQUARE / Grid.SPACING) + 1;
+  OBELISKS_PER_SIDE = (SIZE_SQUARE / SPACING) + 1;
 
   // 3. How close to the edge do we trigger a grid move?
-  var midPoint = Grid.SIZE_SQUARE / 2;
-  if ((Grid.OBELISKS_PER_SIDE % 2) === 0)
+  var midPoint = SIZE_SQUARE / 2;
+  if ((OBELISKS_PER_SIDE % 2) === 0)
   {
     // For even-sized grids the midpoint will always be between two lines.
     // Trigger on the first line between the midpoint and the edge
-    Grid.TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE = midPoint - (Grid.SPACING / 2);
+    TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE = midPoint - (SPACING / 2);
   }
   else
   {
     // For odd-sized grids, the midpoint will be directly on a line of obelisks.
     // Trigger on the next lines over on either side.
-    Grid.TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE = midPoint - Grid.SPACING;
+    TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE = midPoint - SPACING;
   }
-  log('trigger distance from viewport edge is ' + Grid.TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE);
+  log('trigger distance from viewport edge is ' + TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE);
 };
 
 // also takes care of Ground plane
-Grid.addToScene = function()
+function addToScene()
 {
-  scene.add(Grid.mesh); // includes a child Ground object if Ground.DO_RENDER
+  MY3.scene.add(mesh); // includes a child Ground object if Ground.DO_RENDER
   if (!Ground.DO_RENDER)
   {
     Display.groundDiv.style.display = 'block';
   }
 
-  Grid.isActive = true;
+  isActive = true;
 };
 
 // also takes care of Ground plane
-Grid.removeFromScene = function()
+function removeFromScene()
 {
-  scene.remove(Grid.mesh); // includes a child Ground object if Ground.DO_RENDER
+  MY3.scene.remove(mesh); // includes a child Ground object if Ground.DO_RENDER
   if (!Ground.DO_RENDER)
   {
     Display.groundDiv.style.display = 'none';
   }
 
-  Grid.isActive = false;
+  isActive = false;
 };
 
 // start in the viewport centre or offset to avoid a collision if OBELISKS_PER_SIDE is even
-Grid.playerStartLocation = function()
+function playerStartLocation()
 {
-  var position = new THREE.Vector3(Grid.SIZE_SQUARE / 2, Encounter.CAMERA_HEIGHT, Grid.SIZE_SQUARE / 2);
+  var position = new THREE.Vector3(SIZE_SQUARE / 2, Encounter.CAMERA_HEIGHT, SIZE_SQUARE / 2);
   if (Physics.isCloseToAnObelisk(position, Player.RADIUS))
   {
-    position.x += Grid.SPACING / 2;
-    position.z += Grid.SPACING / 2;
+    position.x += SPACING / 2;
+    position.z += SPACING / 2;
   }
   return position;
 };
 
 // returns a Vector3 with X,Z *somewhere in the viewport* and Y=0
-Grid.randomLocation = function()
+function randomLocation()
 {
-  var x = UTIL.random(Grid.viewport.min.x, Grid.viewport.max.x);
-  var z = UTIL.random(Grid.viewport.min.y, Grid.viewport.max.y);
+  var x = UTIL.random(viewport.min.x, viewport.max.x);
+  var z = UTIL.random(viewport.min.y, viewport.max.y);
   return new THREE.Vector3(x, 0, z);
 };
 
 // returns a Vector3.
 // requires a max distance. Optionally can specify a minimum distance
 // FIXME brute force alert!
-Grid.randomLocationCloseToPlayer = function(maxDistance, minDistance)
+function randomLocationCloseToPlayer(maxDistance, minDistance)
 {
   var location = null;
   do
   {
-    location = Grid.randomLocationCloseToPoint(Player.position, maxDistance);
+    location = randomLocationCloseToPoint(Player.position, maxDistance);
   } while (location.distanceTo(Player.position) < minDistance);
   return location;
 };
@@ -140,7 +144,7 @@ Grid.randomLocationCloseToPlayer = function(maxDistance, minDistance)
 // point: Vector3
 // returns a Vector3
 // FIXME brute force alert!
-Grid.randomLocationCloseToPoint = function(point, maxDistance)
+function randomLocationCloseToPoint(point, maxDistance)
 {
   if (point.x === undefined)
   {
@@ -150,53 +154,53 @@ Grid.randomLocationCloseToPoint = function(point, maxDistance)
   var location = null;
   do
   {
-    location = Grid.randomLocation();
+    location = randomLocation();
   } while (point.distanceTo(location) > maxDistance);
   return location;
 };
 
 // reset the position of the viewport, and use the correct colours for this level
-Grid.reset = function()
+reset = function()
 {
-  Grid.viewport.set(new THREE.Vector2(0,0), new THREE.Vector2(Grid.SIZE_SQUARE, Grid.SIZE_SQUARE));
-  Grid.mesh.position.x = Grid.viewport.min.x;
-  Grid.mesh.position.z = Grid.viewport.min.y; // note that Y in the Vector2 represents Z
+  viewport.set(new THREE.Vector2(0,0), new THREE.Vector2(SIZE_SQUARE, SIZE_SQUARE));
+  mesh.position.x = viewport.min.x;
+  mesh.position.z = viewport.min.y; // note that Y in the Vector2 represents Z
 
-  Grid.mesh.material.color = new THREE.Color(Level.current.obeliskColor);
+  mesh.material.color = new THREE.Color(Level.current.obeliskColor);
   Ground.setColor(Level.current.groundColor);
 };
 
 // When the player moves close to the edge of the grid, translate it seamlessly.
 // Child objects (Ground plane) will inherit translations.
 // Reminder, the Grid.mesh is anchored at its bottom left (0,0) in X,Z terms.
-Grid.update = function()
+function update()
 {
   // define a threshold that will trigger when the player crosses it
   // TODO could use another bounding box with contains() to make this tidier
-  var maxThresholdX = Grid.viewport.max.x - Grid.TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE;
-  var maxThresholdZ = Grid.viewport.max.y - Grid.TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE;
-  var minThresholdX = Grid.viewport.min.x + Grid.TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE;
-  var minThresholdZ = Grid.viewport.min.y + Grid.TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE;
+  var maxThresholdX = viewport.max.x - TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE;
+  var maxThresholdZ = viewport.max.y - TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE;
+  var minThresholdX = viewport.min.x + TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE;
+  var minThresholdZ = viewport.min.y + TRIGGER_DISTANCE_FROM_VIEWPORT_EDGE;
 
   if (Player.position.x > maxThresholdX)
   {
-    Grid.viewport.translate(new THREE.Vector2(Grid.SPACING, 0));
+    viewport.translate(new THREE.Vector2(SPACING, 0));
   }
   else if (Player.position.x < minThresholdX)
   {
-    Grid.viewport.translate(new THREE.Vector2(-Grid.SPACING, 0));
+    viewport.translate(new THREE.Vector2(-SPACING, 0));
   }
 
   if (Player.position.z > maxThresholdZ)
   {
-    Grid.viewport.translate(new THREE.Vector2(0, Grid.SPACING));
+    viewport.translate(new THREE.Vector2(0, SPACING));
   }
   else if (Player.position.z < minThresholdZ)
   {
-    Grid.viewport.translate(new THREE.Vector2(0, -Grid.SPACING));
+    viewport.translate(new THREE.Vector2(0, -SPACING));
   }
 
   // move the mesh to match the viewport
-  Grid.mesh.position.x = Grid.viewport.min.x;
-  Grid.mesh.position.z = Grid.viewport.min.y; // note that Y in the Vector2 represents Z
+  mesh.position.x = viewport.min.x;
+  mesh.position.z = viewport.min.y; // note that Y in the Vector2 represents Z
 };
