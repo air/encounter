7c1f4d9ad14731f8fa12d5b3c01a88b21d3498bd
Aaron Bell <aaronbell@meta.com>
2025-12-30 16:10:37 -0500

    fixed for 0.137

--- js/modules/MY3.js
+++ js/modules/MY3.js
@@ -336,61 +336,48 @@ MATS.wireframe = new window.THREE.MeshBasicMaterial({color : 0xFFFFFF, wireframe
 MATS.lineVertex = new window.THREE.LineBasicMaterial( { vertexColors: window.THREE.VertexColors, linewidth: 1 } );
 
 // constructor. Pass an array of colors (hex numbers), and the number of frames to display each.
-export function FlickeringBasicMaterial(hexArray, framesForEach) {
-  if ( !(hexArray instanceof Array) )
-  {
-    panic('hexArray not an Array');
-  }
-  if (typeof(hexArray[0]) !== 'number')
-  {
-    panic('hexArray must contain numbers');
-  }
-  if (hexArray.length < 1)
-  {
-    panic('hexArray is empty');
-  }
-  if (framesForEach === undefined)
-  {
-    panic('framesForEach undefined');
-  }
-
-  // Don't call parent constructor - instead create a proper instance
-  // and copy properties to this
-  const baseMaterial = new window.THREE.MeshBasicMaterial();
-  Object.setPrototypeOf(this, FlickeringBasicMaterial.prototype);
-
-  // Copy all properties from the base material to this
-  Object.assign(this, baseMaterial);
-
-  // config. Create Color objects out of the hex values
-  this.colorArray = hexArray.map(function(color) { return new window.THREE.Color(color); });
-  this.framesForEach = framesForEach;
-
-  // current state
-  this.frameCounter = 0;
-  this.currentColor = 0;
-  this.color = this.colorArray[this.currentColor];
-}
-
-FlickeringBasicMaterial.prototype = Object.create(window.THREE.MeshBasicMaterial.prototype);
-FlickeringBasicMaterial.prototype.constructor = FlickeringBasicMaterial;
-
-FlickeringBasicMaterial.prototype.tick = function() {
-  this.frameCounter += 1;
-  // change the color if needed
-  if (this.frameCounter === this.framesForEach)
-  {
-    this.currentColor += 1;
-    if (this.currentColor === this.colorArray.length)
-    {
-      this.currentColor = 0;
+export class FlickeringBasicMaterial extends window.THREE.MeshBasicMaterial {
+  constructor(hexArray, framesForEach) {
+    if ( !(hexArray instanceof Array) ) {
+      panic('hexArray not an Array');
     }
-    this.color = this.colorArray[this.currentColor];
+    if (typeof(hexArray[0]) !== 'number') {
+      panic('hexArray must contain numbers');
+    }
+    if (hexArray.length < 1) {
+      panic('hexArray is empty');
+    }
+    if (framesForEach === undefined) {
+      panic('framesForEach undefined');
+    }
 
-    // reset counter
+    super();
+
+    // config. Create Color objects out of the hex values
+    this.colorArray = hexArray.map(function(color) { return new window.THREE.Color(color); });
+    this.framesForEach = framesForEach;
+
+    // current state
     this.frameCounter = 0;
+    this.currentColor = 0;
+    this.color = this.colorArray[this.currentColor];
   }
-};
+
+  tick() {
+    this.frameCounter += 1;
+    // change the color if needed
+    if (this.frameCounter === this.framesForEach) {
+      this.currentColor += 1;
+      if (this.currentColor === this.colorArray.length) {
+        this.currentColor = 0;
+      }
+      this.color = this.colorArray[this.currentColor];
+
+      // reset counter
+      this.frameCounter = 0;
+    }
+  }
+}
 
 //=============================================================================
 // colors
